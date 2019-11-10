e3554beb50d7cc9eb96fdb00b3089518623d4900
zdyrko <yuriy.zdyrko@gmail.com>
2019-11-10 09:28:46 +0200

    db setup

--- config/dev.exs
+++ config/dev.exs
@@ -41,6 +41,15 @@ config :conduit, Conduit.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
-  database: "conduit_dev",
+  database: "conduit_readstore_dev",
+  hostname: "localhost",
+  pool_size: 10
+
+# Configure the event store database
+config :eventstore, EventStore.Storage,
+  serializer: Commanded.Serialization.JsonSerializer,
+  username: "postgres",
+  password: "postgres",
+  database: "conduit_eventstore_dev",
   hostname: "localhost",
   pool_size: 10
